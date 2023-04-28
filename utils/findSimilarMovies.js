9594a2427a53b90d290022b547c64fa4002513b1
soundwanders <soundwanders@gmail.com>
2023-04-27 20:18:13 -0400

    Fixed genre query parameter bug

--- utils/findSimilarMovies.js
+++ utils/findSimilarMovies.js
@@ -39,9 +39,10 @@ const findSimilarMovies = async (queryMovie, releaseDate, genreMatches, actorMat
         with_genres: genreMatches && genreMatches.map((genre) => genre.trim()).join(','),
         with_cast: actorMatches && actorMatches.map((actor) => actor.trim()).join(','),
         with_original_language: languageValues && languageValues.map((language) => language.trim()).join(','),
+        page: 1,
+        include_adult: false,
         'primary_release_date.gte': `${minReleaseYear}-01-01`,
         'primary_release_date.lte': `${maxReleaseYear}-12-31`,
-        page: 1,
       },
     });
 
@@ -55,55 +56,57 @@ const findSimilarMovies = async (queryMovie, releaseDate, genreMatches, actorMat
 
       return movie;
     });
+    // Filter our query movie from the API results
+    similarMovies = similarMovies.filter((movie) => movie.id !== queryMovie.id);
 
     // Sort movies based on similarity score in descending order
     similarMovies.sort((a, b) => b.similarityScore - a.similarityScore);
 
-    // Filter our query movie from the API results
-    similarMovies = similarMovies.filter((movie) => movie.id !== queryMovie.id);
-
-    // Define empty array which will be populated with our similar movies
+    // Initialize an empty array, will be populated with the movie data
     let filteredMovies = [];
 
-    // Fetch the list of query movie's genres
     const genreArray = await genreList();
 
     for (const movie of similarMovies) {
-       // Create a flag variable to keep track of which movies should be included in search results
+      // Create a flag variable to keep track of which movies should be included in search results
       let includeMovie = true;
-
+    
       // Check if any of query movie's genre match the user's genre query
       if (genreMatches) {
         const genres = movie.genre_ids.map((id) => genreArray.find((genre) => genre.id === id).name.toLowerCase());
-        const genreMatchesLower = genreMatches.map((genre) => genre.toLowerCase());
-        includeMovie = genreMatchesLower.every((genre) => genres.includes(genre));
-      }
-
-      // Check if any actors/actresses from query movie match user's actor query
-      if (actorMatches && includeMovie) {
-        const movieCredits = await getMovieCredits(movie.id);
-        const movieCast = movieCredits.cast.map((cast) => cast.name.toLowerCase());
-        const actorMatchesLower = actorMatches.map((actor) => actor.toLowerCase());
-        includeMovie = actorMatchesLower.some((actor) => movieCast.includes(actor));
-      }
-
-      // Check if query movie's original language matches user's language query
-      if (languageMatches && includeMovie) {
-        const queryLanguage = languageMatches.map((language) => language.toLowerCase());
-        includeMovie = queryLanguage.includes(movie.original_language.toLowerCase());
-        
-        if (!includeMovie) {
-          const movieDetails = await getMovieLanguage(movie.id);
-          includeMovie = movieDetails.spoken_languages.some((movieLanguage) => {
-            return queryLanguage.includes(movieLanguage.name.toLowerCase());
-          });
+        const genreMatchesLower = genreMatches?.map((genre) => genre.toLowerCase());
+    
+        if (!genreMatchesLower || genreMatchesLower.some((genre) => genres.includes(genre))) {
+          includeMovie = genreMatchesLower.every((genre) => genres.includes(genre));
+    
+          // Check if any actors/actresses from query movie match user's actor query
+          if (actorMatches && includeMovie) {
+            const movieCredits = await getMovieCredits(movie.id);
+            const movieCast = movieCredits.cast.map((cast) => cast.name.toLowerCase());
+            const actorMatchesLower = actorMatches.map((actor) => actor.toLowerCase());
+            includeMovie = actorMatchesLower.some((actor) => movieCast.includes(actor));
+          }
+    
+          // Check if query movie's original language matches user's language query
+          if (languageMatches && includeMovie) {
+            const queryLanguage = languageMatches.map((language) => language.toLowerCase());
+            includeMovie = queryLanguage.includes(movie.original_language.toLowerCase());
+    
+            if (!includeMovie) {
+              const movieDetails = await getMovieLanguage(movie.id);
+              includeMovie = movieDetails.spoken_languages.some((movieLanguage) => {
+                return queryLanguage.includes(movieLanguage.name.toLowerCase());
+              });
+            }
+          }
         }
       }
-
+    
+      // If the movie matches all search criteria, add it to the results
       if (includeMovie) {
         filteredMovies.push(movie);
       }
-    };
+    }
 
     if (filteredMovies.length === 0) {
       return null;
--- utils/genreList.js
+++ utils/genreList.js
@@ -4,13 +4,18 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const TMDB_API_URL = process.env.TMDB_API_URL || 'https://api.themoviedb.org/3';
 
 async function genreList() {
-  const response = await axios.get(`${TMDB_API_URL}/genre/movie/list`, {
-    params: {
-      api_key: TMDB_API_KEY,
-      language: 'en-US',
-    },
-  });
-  return response.data.genres;
+  try {
+    const response = await axios.get(`${TMDB_API_URL}/genre/movie/list`, {
+      params: {
+        api_key: TMDB_API_KEY,
+        language: 'en-US',
+      },
+    });
+    return response.data.genres;
+  } catch (error) {
+    console.error(`Error getting movie credits for movie ID ${movieId}: ${error.message}`);
+    return null;
+  }
 }
 
 module.exports = { 
